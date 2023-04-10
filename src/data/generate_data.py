5e8088b1b75cf39e6954f444a4fdd2f6ca9e6012
Marco <mpinheiro19@gmail.com>
2023-04-09 21:21:28 -0300

    initial commit

---
+++ src/data/generate_data.py
@@ -0,0 +1,33 @@
+import datetime as dt
+
+import numpy as np
+import pandas as pd
+from numpy.random import default_rng
+
+# Setting random seed value
+SEED = 42
+
+# Set the number of distinct features
+N = 10
+
+# Instanciate numpy's random generator class
+rng = default_rng(SEED)
+
+
+# Set the time range
+start_date = dt.datetime(2022, 1, 1)
+end_date = dt.datetime(2022, 12, 31)
+date_range = pd.date_range(start=start_date, end=end_date, freq="D")
+
+# Create a random time series with N distinct features
+data = rng.integers(
+    low=0, high=65000, size=(len(date_range), N), endpoint=True
+)
+
+# np.random.randn(len(date_range), N)
+
+# Create a Pandas DataFrame with the time series data
+df = pd.DataFrame(
+    data, index=date_range, columns=["ft_" + str(i + 1) for i in range(N)]
+)
+df.to_csv("./data/raw/fake_series.csv")
